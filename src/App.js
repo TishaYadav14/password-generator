33a568c9733bec302400fc759be16871687f4837
Tisha Yadav <tyadhuvanshi1408@gmail.com>
2024-04-28 03:41:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import usePasswordGenerator from "./Hooks/use-password-generator";
+import PasswordStrengthIndicator from "./Components/PasswordStrengthIndicator";
 
 function App() {
+  const [length, setLength] = useState(4);
+  const [checkboxData, setCheckboxData] = useState([
+    { title: "Include Uppercase Letters", state: false },
+    { title: "Include Lowercase Letters", state: false },
+    { title: "Include Numbers", state: false },
+    { title: "Include Symbols", state: false },
+  ]);
+  const [copied, setCopied] = useState(false);
+
+  const handleCheckboxChange = (i) => {
+    const updatedCheckboxData = [...checkboxData];
+    updatedCheckboxData[i].state = !updatedCheckboxData[i].state;
+    setCheckboxData(updatedCheckboxData);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+  };
+
+  const { password, errorMessage, generatePassword } = usePasswordGenerator();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {password && (
+        <div className="header">
+          <div className="title">{password}</div>
+          <button className="copyBtn" onClick={handleCopy}>
+            {copied ? "Copied" : "Copy"}
+          </button>
+        </div>
+      )}
+
+      <div className="charlength">
+        <span>
+          <label>Character Length</label>
+          <label>{length}</label>
+        </span>
+        <input
+          type="range"
+          min="4"
+          max="20"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+        />
+      </div>
+
+      <div className="checkboxes">
+        {checkboxData.map((checkbox, index) => {
+          return (
+            <div key={index}>
+              <input
+                type="checkbox"
+                checked={checkbox.state}
+                onChange={() => handleCheckboxChange(index)}
+              />
+              <label>{checkbox.title}</label>
+            </div>
+          );
+        })}
+      </div>
+
+      {/* strength */}
+      <PasswordStrengthIndicator password={password} />
+
+      {/* error handling */}
+      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+
+      <button
+        className="generateBtn"
+        onClick={() => generatePassword(checkboxData, length)}
+      >
+        Generate Password
+      </button>
     </div>
   );
 }
